b3a0f4579f8a37cea79ea93d057fd46a87fb6ad2
resianlucas <101301367+resianlucas@users.noreply.github.com>
2023-04-09 21:04:31 -0300

    Projeto API SpringBoot já finalizado.
    CRUD completo.

---
+++ SpringBoot/api/src/main/java/med/voll/api/paciente/DadosListagemPaciente.java
@@ -0,0 +1,11 @@
+package med.voll.api.paciente;
+
+import med.voll.api.paciente.Paciente;
+
+public record DadosListagemPaciente(Long id, String nome, String email, String cpf, String telefone) {
+
+    public DadosListagemPaciente(Paciente paciente) {
+        this(paciente.getId(), paciente.getNome(), paciente.getEmail(), paciente.getCpf(), paciente.getTelefone());
+    }
+}
+
